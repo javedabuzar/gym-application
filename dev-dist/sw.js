3c9e2f802504c3d29970825ce3f1f128dc63de67
javedabuzar <javedabuzar24@gmail.com>
2026-03-26 18:08:37 +0500

    Final PWA installation and UI improvements for Vercel deployment

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.obcdc5cjeug"
+    "revision": "0.794dtk29ej4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
