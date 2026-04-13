a0e07a047b6dd041875ec08e043a2c0106490e97
javedabuzar <javedabuzar24@gmail.com>
2026-04-14 00:55:02 +0500

    chore: final cleanup for live release

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bmge9ri5vjo"
+    "revision": "0.b20njamah7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
